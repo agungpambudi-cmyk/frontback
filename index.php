7c8130283a647f29d426c3cea2ae7372a0dcb8d4
agungpambudi-cmyk <hita56164@gmail.com>
2020-11-23 14:16:23 +0700

    project pertama

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document1</title>
+    <script src="https://kit.fontawesome.com/6e53ca7139.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <img class="logoku" src="image/logo.png" alt="logo">
+        <nav>
+            <ul class="nav__links">
+                <li><a href="#" class="okokok">Promo</a></li>
+                <li><a href="#" class="okokok">Product</a></li>
+                <li><a href="#" class="okokok">About</a></li>
+            </ul>
+        </nav>
+        <a class="cta" href="login.html"><button>Login</button></a>
+    </header>
+    <div class="container">
+        <div class="card">
+            <div class="shoeBackground">
+                <div class="gradients">
+                    <div class="gradient second" color="purple"></div>
+                    <div class="gradient" color="blue"></div>
+                    <div class="gradient" color="green"></div>
+                </div>
+                <h5 class="nike">Gaun</h5>
+                <img src="image/lambang.png" alt="" class="logo">
+                <a href="" class="share"><i class="fas fa-share-alt"></i></a>
+
+                <img src="image/purple.png" alt="" class="shoe show" color="purple">
+                <img src="image/blue.png" alt="" class="shoe" color="blue">
+                <img src="image/green.png" alt="" class="shoe" color="green">
+
+            </div>
+            <div class="info">
+                <div class="shoeName">
+                    <div>
+                        <h1 class="big">Gaun wanita</h1>
+                        <span class="new">new</span>
+                    </div>
+                    <h3 class="small">Gaun wanita islami</h3>
+                </div>
+                
+                <div class="color-container">
+                    <h3 class="title">Color</h3>
+                    <div class="colors">
+                        <span class="color active" primary="#cc0099" color="purple"></span>
+                        <span class="color" primary="#0136af" color="blue"></span>
+                        <span class="color" primary="#339966" color="green"></span>
+                    </div>
+                </div>
+
+                <div class="description">
+                    <h3 class="title">Product Info</h3>
+                    <p class="text">Sangat dianjurkan untuk membeli gaun ini<br>NOTE:Bukan untuk pria wkwk</p>
+                </div>
+
+                <div class="size-container">
+                    <h3 class="title">size</h3>
+                    <div class="sizes">
+                        <span class="size">S</span>
+                        <span class="size">L</span>
+                        <span class="size active">M</span>
+                        <span class="size">XL</span>
+                        <span class="size">XXL</span>
+                    </div>
+                </div>
+                <div class="buy-price">
+                    <a href="#" class="buy">Add to card<i class="fas fa-shopping-cart"></i></a>
+                    <div class="price">
+                        <i class="fas fa-dollar-sign"></i>
+                        <h1>79.99</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="style.js"></script>
+</body>
+</html>
